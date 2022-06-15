bc9884181fcddcdb6be8fc94d6cec256e017e6bd
cgirod3 <carlosegirod@gmail.com>
2022-06-15 15:45:09 -0400

    move containers and dependencies to appropriate dirs

--- src/index.js
+++ src/index.js
@@ -6,23 +6,18 @@ import {
   Route,
 } from "react-router-dom";
 
-import App from './App';
+import Home from './containers/Home';
+
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <App /> } />
+        <Route path='/' element={ <Home /> } />
 
         {/* <Route path='teams' element={<Teams />}>
           <Route path=':teamId' element={<Team />} />
