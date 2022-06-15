13bf9533bde6d6978c23b42d8443aa5b113a851b
cgirod3 <carlosegirod@gmail.com>
2022-06-15 15:22:59 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <App /> } />
+
+        {/* <Route path='teams' element={<Teams />}>
+          <Route path=':teamId' element={<Team />} />
+          <Route path='new' element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route> */}
+
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
